790dd3be407b280c80042a0f5a5b267eccf2c3d9
Fadlullah Khan <fadlullah.khan007@gmail.com>
2025-02-02 20:11:42 +0700

    Menambahkan Data Validation

---
+++ src/validator/notes/index.js
@@ -0,0 +1,13 @@
+const { NotesPayloadSchema } = require('./schema');
+
+const NotesValidator = {
+  validateNotePayload: payload => {
+    const validationResult = NotesPayloadSchema.validate(payload);
+
+    if (validationResult.error) {
+      throw new Error(validationResult.error.message);
+    }
+  }
+};
+
+module.exports = NotesValidator
